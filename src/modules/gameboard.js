180081f7a4e221cc93bf2d7f62a5f6ff36bd4bda
Kenref <107287396+Kenref@users.noreply.github.com>
2023-08-31 14:26:20 +1000

    removed old code

--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -48,34 +48,4 @@ export default function gameBoardFactory(rows,columns,shipFactory) {
     checkSunkAll: checkSunkAll
   };
 
-  // return {
-  //   grid: grid,
-  //   shipsArray: shipsArray,
-
-  //   placeShip: function (length, x, y) {
-  //     let newShip = ship(length)
-  //     shipsArray.push(newShip)
-  //     for (let i = 0; i < newShip.length; i++) {
-  //       grid[x + i][y] = "ship";
-  //     }
-  //     return newShip
-  //     // allow ships to be placed horizontally or vertically
-  //     // ships cannot be placed if they are longer than board e.g. on the edge
-  //   },
-
-  //   receiveAttack: function(x, y) {
-  //     if (grid[x][y] === "ship") {
-  //       grid[x][y] = "hit"
-  //       return "hit"
-  //     } else {
-  //       grid[x][y] = "miss"
-  //       return "miss"
-  //     }
-  //   },
-
-  //   checkSunkAll: function () {
-  //     return shipsArray.every((ship) => ship.isSunk())
-  //   },
-
-  // }
 }
